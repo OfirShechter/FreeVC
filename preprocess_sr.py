754fd43951641480010191ea1111c5dcb20c3017
OfirShechter <ofir1847@gmail.com>
2025-02-17 23:37:56 +0200

    update

--- preprocess_sr.py
+++ preprocess_sr.py
@@ -10,14 +10,14 @@ from scipy.io import wavfile
 import utils
 from mel_processing import mel_spectrogram_torch
 from wavlm import WavLM, WavLMConfig
-#import h5py
+# import h5py
 import logging
 logging.getLogger('numba').setLevel(logging.WARNING)
 
 
 def process(filename):
     basename = os.path.basename(filename)
-    speaker = filename.split("/")[-2]#basename[:4]
+    speaker = filename.split(os.sep)[-2]  # basename[:4]
     wav_dir = os.path.join(args.wav_dir, speaker)
     ssl_dir = os.path.join(args.ssl_dir, speaker)
     os.makedirs(wav_dir, exist_ok=True)
@@ -25,13 +25,13 @@ def process(filename):
     wav, _ = librosa.load(filename, sr=hps.sampling_rate)
     wav = torch.from_numpy(wav).unsqueeze(0).cuda()
     mel = mel_spectrogram_torch(
-        wav, 
-        hps.n_fft, 
-        hps.num_mels, 
-        hps.sampling_rate, 
-        hps.hop_size, 
-        hps.win_size, 
-        hps.fmin, 
+        wav,
+        hps.n_fft,
+        hps.num_mels,
+        hps.sampling_rate,
+        hps.hop_size,
+        hps.win_size,
+        hps.fmin,
         hps.fmax
     )
     '''
@@ -43,24 +43,24 @@ def process(filename):
     for i in range(args.min, args.max+1):
         mel_rs = utils.transform(mel, i)
         wav_rs = vocoder(mel_rs)[0][0].detach().cpu().numpy()
-        _wav_rs = librosa.resample(wav_rs, orig_sr=hps.sampling_rate, target_sr=args.sr)
+        _wav_rs = librosa.resample(
+            wav_rs, orig_sr=hps.sampling_rate, target_sr=args.sr)
         wav_rs = torch.from_numpy(_wav_rs).cuda().unsqueeze(0)
         c = utils.get_content(cmodel, wav_rs)
         ssl_path = os.path.join(ssl_dir, basename.replace(".wav", f"_{i}.pt"))
         torch.save(c.cpu(), ssl_path)
-        #print(wav_rs.size(), c.size())
+        # print(wav_rs.size(), c.size())
         wav_path = os.path.join(wav_dir, basename.replace(".wav", f"_{i}.wav"))
         wavfile.write(
-                wav_path,
-                args.sr,
-                _wav_rs
+            wav_path,
+            args.sr,
+            _wav_rs
         )
     '''
         f[i][basename[:-4]] = c.cpu()
     for i in range(args.min, args.max+1):
         f[i].close()
     '''
-        
 
 
 if __name__ == "__main__":
@@ -68,10 +68,14 @@ if __name__ == "__main__":
     parser.add_argument("--sr", type=int, default=16000, help="sampling rate")
     parser.add_argument("--min", type=int, default=68, help="min")
     parser.add_argument("--max", type=int, default=92, help="max")
-    parser.add_argument("--config", type=str, default="hifigan/config.json", help="path to config file")
-    parser.add_argument("--in_dir", type=str, default="dataset/vctk-22k", help="path to input dir")
-    parser.add_argument("--wav_dir", type=str, default="dataset/sr/wav", help="path to output wav dir")
-    parser.add_argument("--ssl_dir", type=str, default="dataset/sr/wavlm", help="path to output ssl dir")
+    parser.add_argument(
+        "--config", type=str, default="hifigan/config.json", help="path to config file")
+    parser.add_argument("--in_dir", type=str,
+                        default="dataset/vctk-22k", help="path to input dir")
+    parser.add_argument("--wav_dir", type=str,
+                        default="dataset/sr/wav", help="path to output wav dir")
+    parser.add_argument("--ssl_dir", type=str,
+                        default="dataset/sr/wavlm", help="path to output ssl dir")
     args = parser.parse_args()
 
     print("Loading WavLM for content...")
@@ -86,15 +90,14 @@ if __name__ == "__main__":
     vocoder = utils.get_vocoder(0)
     vocoder.eval()
     print("Loaded vocoder.")
-    
+
     config_path = args.config
     with open(config_path, "r") as f:
         data = f.read()
     config = json.loads(data)
     hps = utils.HParams(**config)
-
-    filenames = glob(f'{args.in_dir}/*/*.wav', recursive=True)#[:10]
-    
+    print(args.in_dir)
+    filenames = glob(f'{args.in_dir}/*/*.wav', recursive=True)  # [:10]
+    print(f"Processing {len(filenames)} files...")
     for filename in tqdm(filenames):
         process(filename)
-    
